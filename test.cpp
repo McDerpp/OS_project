f87651b21b149a04b37df22f3e1e52002213483e
McDerpp <89364235+McDerpp@users.noreply.github.com>
2024-07-08 09:56:36 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -13,12 +13,7 @@ int num_input = 0;
 
 //=====================================================================
 
-struct data_display
-{
-    int process_number;
-    int from;
-    int to;
-};
+
 
 struct data_FCFS
 {
@@ -27,8 +22,7 @@ struct data_FCFS
     int process_number;
     int cpu_burst;
     int arrival_time;
-    int started_at;
-    int ended_at;
+
 };
 
 struct data_SJF
@@ -47,6 +41,17 @@ struct data_PSSJFAT
     int priority_number;
     int started_at;
     int ended_at;
+};
+
+
+struct data_display
+{
+    struct data_FCFS fcfs_data;
+    int process_number;
+    int from;
+    int to;
+    
+    
 };
 
 //=====================================================================
@@ -78,8 +83,7 @@ void initialize_input_FCFS()
         printf("\n");
 
 
-        data_input_FCFS[ctr].started_at = 0;
-        data_input_FCFS[ctr].ended_at = 0;
+
     }
 
     data_input_FCFS[ctr + 1].process_number = 0;
@@ -102,7 +106,18 @@ void display_result()
     // 0 value is waiting time.
     for (int ctr = 0; ctr < display_ctr-1; ctr++)
     {
-        printf("\n Process --> %d", display[ctr].process_number);
+       
+        printf("\n\n------------------------ Processing[%d]------------------------", ctr);
+        if(display[ctr].fcfs_data.process_number != 100){
+        printf("\n Process Number-> %d", display[ctr].fcfs_data.process_number);
+        }
+        else
+        printf("\n CPU Burst Time left-> %d", display[ctr].fcfs_data.cpu_burst);
+        printf("\n Started -> %d", display[ctr].from);
+        printf("\n ended --> %d", display[ctr].to);
+        
+
+        
     }
 }
 
@@ -122,19 +137,15 @@ void simulate_FCFS()
         // checking if its done...
         while (data_input_FCFS[ctr].process_number != 0)
         {
-            
             if (data_input_FCFS[ctr].cpu_burst != 0)
             {
-  
                 min_arrival_time = data_input_FCFS[ctr].arrival_time ;
-
                 break;
             }
             if (data_input_FCFS[ctr].process_number == 0)
             {
                 done = true;
             }
-
             ctr++;
         }
         
@@ -142,9 +153,6 @@ void simulate_FCFS()
         ctr = 0;
         
         
-
-
-
         // checking what process should be next
         while (data_input_FCFS[ctr].process_number != 0)
         {
@@ -162,23 +170,33 @@ void simulate_FCFS()
         // checking elapsed time if a procces had arrived 
         // adding another process for waiting time
         if(elapsed_time < min_arrival_time ){
-            display_ctr++;
+            
         
         // process number 100 signifies a waiting time/gap in between processes(if it occurs)
-        display[display_ctr].process_number = 100;
-            
-            
+        display[display_ctr].fcfs_data.process_number = 100;
+        // it starts at the current elapsed time
+        display[display_ctr].from = elapsed_time;
+        
+        
+        // elapsed time added with the process next in line to process to simulate waiting
+        // suppose current elapsed time is 0 and next process arrival time is 5, therefore
+        // 0 + 5 -> elapsed time = 5 and the next process starts at 5(explained in the next part)
+        elapsed_time += elapsed_time + min_arrival_time;
+        display[display_ctr].to = elapsed_time;
+        
+        // for displaying ctr
+        display_ctr++;
         }
         
 
- 
-        
 
-        // simulates doing the process by decrementing the cpu burst of the process into 0
-        // elapsed time is then incremented signifying the current time after the processes
-     
+        display[display_ctr].fcfs_data = data_input_FCFS[processing];
+        // tracking of what process number is being processed
         display[display_ctr].process_number = data_input_FCFS[processing].process_number;
- 
+        
+         // simulates doing the process by decrementing the cpu burst of the process into 0
+        // elapsed time is then incremented signifying the current time after the processes
+        
         // recording from - this is before process is done
         display[display_ctr].from = elapsed_time;
         // recording to - this is after the process is done
